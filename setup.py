dc9fa8090a4cd772fc5781122aa8c9eafc01add2
Leo Ueno <leo@roboflow.com>
2024-05-10 09:27:42 -0700

    Added a setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="OCR Checkup",
+    version="0.1.0",
+    description="Test OCR Models!",
+    author="Leo Ueno",
+    author_email="leo@roboflow.com",
+    packages=find_packages(),
+    install_requires=[],
+)
